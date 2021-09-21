8770d487494cd18a34d19ff6a05f2b747bcea30a
Maulana-Pardip <90297349+Maulana-Pardip@users.noreply.github.com>
2021-09-21 10:46:45 +0700

    Add files via upload

---
+++ Jajar Genjang.py
@@ -0,0 +1,7 @@
+print ("Jajar Genjang")
+n = int (input("Masukkan  nilai n: "))
+i = 1
+a = n
+while (i<=n):
+    print (" "*(n-1), "*" * a)
+    n = n-1
---
+++ Luas Segitiga.py
@@ -0,0 +1,7 @@
+print ("Menghitung Luas Ssegitiga")
+a = float (input("Masukkan panjang alas = "))
+t = float (input("Masukkan panjang tinggi = "))
+
+luasSegitiga = 0.5*a*t
+
+print ("Luas Segitiga adalah ", luasSegitiga)
